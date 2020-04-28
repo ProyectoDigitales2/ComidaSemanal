a08fde8bead4b3166800b128599b5fff965091a1
Roger Avilés G <rogiavil@espol.edu.ec>
2020-04-28 13:03:56 -0500

    Muestra el listado de las comida por fecha.

--- src/Controlador/VistaListadoController.java
+++ src/Controlador/VistaListadoController.java
@@ -5,11 +5,15 @@
  */
 package Controlador;
 
+import Modelo.Estatico;
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.TableView;
 
 /**
  * FXML Controller class
@@ -18,6 +22,18 @@ import javafx.fxml.Initializable;
  */
 public class VistaListadoController implements Initializable {
 
+    @FXML private TableView tbl_comida;
+    @FXML private TableView tbl_ingrdt;
+    @FXML private DatePicker dp_desde;
+    @FXML private DatePicker dp_hasta;
+
+    private final String QueyComida = "select c.nombre as nombre \n"
+            + "from comida c, fecha_comida fc \n"
+            + "where c.id_comida= fc.id_comida and fc.fecha between '";
+    private final String QueryIngrediente="select distinct i.nombre from ingrediente i, comida c, plato pl , fecha_comida fc \n" +
+"where c.id_comida= fc.id_comida and pl.id_comida=c.id_comida and i.id_ingrediente=pl.id_ingrediente\n" +
+" and fc.fecha between '";
+
     /**
      * Initializes the controller class.
      */
@@ -25,9 +41,21 @@ public class VistaListadoController implements Initializable {
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
     }    
-
-    @FXML
-    private void dp_fecha_listado(ActionEvent event) {
+    
+    @FXML private void action_btnDesde(ActionEvent event) {
+        if(dp_desde.getValue()!= null){
+            Estatico.obtenerTablaDinamica(-1, QueyComida+dp_desde.getValue()+"' and DATE_ADD( '"+dp_desde.getValue()+"' , INTERVAL 7 DAY)", tbl_comida, "tbl_comida");
+            Estatico.obtenerTablaDinamica(-1, QueryIngrediente+dp_desde.getValue()+"' and DATE_ADD( '"+dp_desde.getValue()+"' , INTERVAL 7 DAY)", tbl_ingrdt, "tbl_ingrd");
+        }
+        
     }
+
+    @FXML private void action_btntodo(ActionEvent event) {
+        if(dp_desde.getValue()!= null && dp_hasta.getValue()!= null){
+            Estatico.obtenerTablaDinamica(-1, QueyComida+dp_desde.getValue()+"' and '"+dp_hasta.getValue()+"' ;", tbl_comida, "tbl_comida");
+            Estatico.obtenerTablaDinamica(-1, QueryIngrediente+dp_desde.getValue()+"' and '"+dp_hasta.getValue()+"' ;", tbl_ingrdt, "tbl_ingrd");
+        }
+    }
+
     
 }
